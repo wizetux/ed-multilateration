778c7d5af03fd68ad7bbb5847fe4d6fa535beb7f
Wizetux <wizetux@gmail.com>
2023-04-16 21:43:08 -0700

    main: Do not allow empty input from the user

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use reqwest::Error;
 use std::collections::HashSet;
 use std::cmp::Eq;
 use futures::join;
-use std::io;
+use std::io::{self, Write};
 
 #[derive(Deserialize, Debug)]
 struct System {
@@ -41,27 +41,29 @@ async fn main() -> Result<(), Error> {
 }
 
 fn get_systems_and_range() -> (String, String, String) {
-    let mut system_name = String::new();
-    let mut min_radius = String::new();
-    let mut max_radius = String::new();
-    println!("System Name:");
-    io::stdin()
-        .read_line(&mut system_name)
-        .expect("Failed to read line");
-
-    println!("Min Radius:");
-    io::stdin()
-        .read_line(&mut min_radius)
-        .expect("Failed to read line");
-
-    println!("Max Radius:");
-    io::stdin()
-        .read_line(&mut max_radius)
-        .expect("Failed to read line");
-
-    (system_name.trim().to_string(), min_radius.trim().to_string(), max_radius.trim().to_string())
+    let system_name = get_input_from_user("System Name:".to_string(), "Failed to read line".to_string(), "Please enter a valid system name.".to_string());
+    let min_radius = get_input_from_user("Min Radius:".to_string(), "Failed to read line".to_string(), "Please enter a valid min radius".to_string());
+    let max_radius = get_input_from_user("Max Radius:".to_string(), "Failed to read line".to_string(), "Please enter a valid max radius".to_string());
+    (system_name, min_radius, max_radius)
 }
 
+fn get_input_from_user(input_string: String, error_string: String, empty_string: String) -> String {
+    let mut input = String::new();
+    while input.is_empty() {
+        io::stdout().write_all(&input_string.as_bytes()).expect("Failed to write input string");
+        io::stdout().flush().expect("failed");
+        io::stdin()
+            .read_line(&mut input)
+            .expect(&error_string);
+
+        input = input.trim().to_string();
+        if input.is_empty() {
+            println!("{}", empty_string)
+        }
+    }
+
+    return input;
+}
 async fn get_bodies_in_sphere(system_name: String, min_radius: String, radius: String) -> Result<Vec<System>, Error> {
     let client = reqwest::Client::new();
     let response = client.get("https://www.edsm.net/api-v1/sphere-systems")
